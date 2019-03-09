98781252d3d67e1df155d0a6034956a84a394fb6
9hy <ggfrangg@gmail.com>
2019-03-09 01:56:45 +0100

    small changes

--- XBF/Properties/AssemblyInfo.cs
+++ XBF/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XFB")]
+[assembly: AssemblyTitle("XFaceBlur")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("xliee")]
 [assembly: AssemblyProduct("XFB")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright xliee ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
